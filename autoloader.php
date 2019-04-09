94789109f8cd85b7d93be1989b9ce92dc345bfa8
Julia <yliamala19861@gmail.com>
2019-04-09 14:36:22 +0300

    Removed PriceDrink class

--- autoloader.php
+++ autoloader.php
@@ -2,7 +2,7 @@
 
 spl_autoload_register(function ($class) {
     $parts = str_replace('\\', DIRECTORY_SEPARATOR, $class);
-    $filePath = __DIR__ . DIRECTORY_SEPARATOR . $parts . '.php'; // @todo rename all files by PSR-4
+    $filePath = __DIR__ . DIRECTORY_SEPARATOR . $parts . '.php';
 
     if (!file_exists($filePath)) {
         throw new Exception(sprintf("Class %s not exist by %s path", $class, $filePath));
